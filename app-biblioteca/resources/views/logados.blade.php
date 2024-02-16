21f05fb28550f4390bc373cacbd4fdbba98aa1aa
ManuRguez <mrodpor884@iescarrillo.es>
2024-02-16 12:30:03 +0100

    subida pagina principal

--- app-biblioteca/resources/views/logados.blade.php
+++ app-biblioteca/resources/views/logados.blade.php
@@ -41,14 +41,46 @@
  <!-- Contenido principal -->
 <!-- Contenido principal -->
 <main role="main" class="col-md-9 ml-sm-auto col-lg-10 px-md-4">
-    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Bienvenido a Biblioteca Carrillo</h1>
-    </div>
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
 
-    <img src="{{ asset('images/pantallaPrincipal.jpeg') }}"" alt="Imagen Biblioteca Carrillo" class="img-fluid mb-3">
+        <div class="text-center">
+            
+               
+            </div>
 
-    <h4>Es un placer darle la bienvenida a la Biblioteca Carrillo, un espacio dedicado al conocimiento, la cultura y el enriquecimiento personal. Nos complace enormemente tener la oportunidad de compartir con usted la diversidad de recursos y servicios que ofrecemos.</h4>
+            <div class="text-center">
+                <h4>Es un placer darle la bienvenida a la Biblioteca Carrillo, un espacio dedicado al conocimiento, la cultura y el enriquecimiento personal. Nos complace enormemente tener la oportunidad de compartir con usted la diversidad de recursos y servicios que ofrecemos.</h4></br>
 
+                </br>
+                
+                <img src="{{ asset('images/pantallaPrincipal.jpeg') }}" alt="Imagen Biblioteca Carrillo" class="img-fluid mb-3"></br>
+
+                </br>
+
+                <h4>
+                    En la Biblioteca Carrillo, nos esforzamos por ser mucho más que un simple depósito de libros; somos un centro dinámico que fomenta la exploración intelectual, el aprendizaje continuo y el desarrollo de la comunidad. Nuestra colección abarca una amplia gama de temas, desde literatura clásica hasta las últimas novedades en ciencia, tecnología, historia y más.</br>
+
+                </br>
+                    Además de nuestros recursos impresos, también ofrecemos acceso a plataformas digitales, computadoras con conexión a Internet y eventos culturales. Nuestro personal altamente capacitado está siempre disponible para ayudarlo a encontrar la información que busca, recomendarle lecturas interesantes o guiarlo en el uso de nuestras instalaciones.</br>
+
+                    </br>
+
+                    En la Biblioteca Carrillo, nos enorgullece ser un lugar inclusivo que promueve la diversidad de ideas y perspectivas. Invitamos a personas de todas las edades y trasfondos a explorar y aprovechar al máximo los servicios que ofrecemos.</br>
+
+                    </br>
+
+                    Le animamos a participar en nuestros eventos, actividades y programas, diseñados para inspirar la curiosidad y fomentar la conexión dentro de nuestra comunidad. Creemos firmemente que la biblioteca es un espacio vital donde las ideas convergen y las mentes se expanden.</br>
+
+                    </br>
+                    Estamos comprometidos a brindarle una experiencia bibliotecaria excepcional, y esperamos que encuentre en la Biblioteca Carrillo un lugar donde su búsqueda de conocimiento sea gratificante y enriquecedora.</br>
+
+                    </br>
+
+                    Gracias por elegirnos como su destino bibliotecario. </br>
+                    </br>
+                    ¡Bienvenido/a a la Biblioteca Carrillo!
+                </h4>
+            </div>
     <div class="row justify-content-center">
         <div class="col-md-8">
         </div>
